e94f75b4754bba75c26ffe6cc92c204089e2ddd0
kenkoolio <kensoko0l@yahoo.com>
2016-12-02 11:59:16 -0600

    sessions login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def index
   end
+
+  def create #login
+    @user = User.find_by(email: user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(session[:user_id]) #user dashboard, users::show(:id)
+    else
+      flash[:errors] = ['Invalid email or password']
+      redirect_to '/'
+    end
+  end
+
+  def destroy #logout
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+  private
+  def user_params
+    params.require(:sessions).permit(:email, :password)
+  end
 end
