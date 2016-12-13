3e0123fd87c03c94f4511beec211a990bbf15a58
kenkoolio <kensoko0l@yahoo.com>
2016-12-13 14:17:45 -0600

    Edited the service associations

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,9 +2,9 @@ class Service < ActiveRecord::Base
   has_many :addresses
   has_many :contacts
 
-  has_many :types, through: :service_to_type, source: :type
-  has_many :docs, through: :service_to_doc, source: :required_doc
-  has_many :affiliations, through: :service_to_affiliation, source: :affiliation
+  has_many :types, through: :service_to_type
+  has_many :required_docs, through: :service_to_doc
+  has_many :affiliations, through: :service_to_affiliation
 
 
   validates :name, :description, :website, presence: true
