9164a84e0333eda9a0933b9fb495b9813a6f277d
kenkoolio <kensoko0l@yahoo.com>
2016-12-13 15:18:13 -0600

    created Contact model

---
+++ app/models/contact.rb
@@ -0,0 +1,11 @@
+class Contact < ActiveRecord::Base
+  belongs_to :service
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  validates :phone, presence: true
+
+  #todo - before save/create, make phone a string of only numbers (remove special characters)
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201222730) do
+ActiveRecord::Schema.define(version: 20161213210834) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "number"
@@ -27,10 +27,82 @@ ActiveRecord::Schema.define(version: 20161201222730) do
   add_index "addresses", ["state_id"], name: "index_addresses_on_state_id"
   add_index "addresses", ["zip_id"], name: "index_addresses_on_zip_id"
 
+  create_table "affiliations", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer  "service_id"
+    t.string   "name"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "contacts", ["service_id"], name: "index_contacts_on_service_id"
+
+  create_table "required_docs", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "document"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "service_to_affiliations", force: :cascade do |t|
+    t.integer  "service_id"
+    t.integer  "affiliation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "service_to_affiliations", ["affiliation_id"], name: "index_service_to_affiliations_on_affiliation_id"
+  add_index "service_to_affiliations", ["service_id"], name: "index_service_to_affiliations_on_service_id"
+
+  create_table "service_to_docs", force: :cascade do |t|
+    t.integer  "service_id"
+    t.integer  "required_doc_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "service_to_docs", ["required_doc_id"], name: "index_service_to_docs_on_required_doc_id"
+  add_index "service_to_docs", ["service_id"], name: "index_service_to_docs_on_service_id"
+
+  create_table "service_to_types", force: :cascade do |t|
+    t.integer  "service_id"
+    t.integer  "type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "service_to_types", ["service_id"], name: "index_service_to_types_on_service_id"
+  add_index "service_to_types", ["type_id"], name: "index_service_to_types_on_type_id"
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "website"
+    t.text     "hours"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "states", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
